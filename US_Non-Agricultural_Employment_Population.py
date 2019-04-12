2958fa3e9436a456d0f233166d5623a3eff25f5f
jp6ru8958958 <jp6ru8958958@gmail.com>
2019-04-13 04:04:13 +0800

    US_NAE crawler

--- US_Non-Agricultural_Employment_Population.py
+++ US_Non-Agricultural_Employment_Population.py
@@ -1,10 +1,20 @@
 #美國非農就業人口
 import requests
 from bs4 import BeautifulSoup
-url='https://stock-ai.com/eom-1-PAYEMS.php'
+url='https://tradingeconomics.com/united-states/non-farm-payrolls'
 Response=requests.get(url)
 Response.encoding='Utf-8'
-#print(Response.status_code)
+print(Response.status_code)
 #print(Response.text)
 soup=BeautifulSoup(Response.text,"html.parser")
-#find=soup.select("div.pic2t.pic2t_bg a")
+find=soup.select("td.datatable-item.datatable-item-positive")
+
+for blank_scan in range(len(find)):
+    if find[blank_scan] == '''<td class="datatable-item datatable-item-positive" id="actual"></td>''':
+        find[blank_scan].text = "None"
+
+for row in range(7):
+    print("---")
+    print_=row*4
+    print(str.strip(find[print_].text),str.strip(find[print_+1].text),str.strip(find[print_+2].text),str.strip(find[print_+3].text))
+print("---")
