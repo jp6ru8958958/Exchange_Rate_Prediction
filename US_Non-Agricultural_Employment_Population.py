c0f88ab3f36bef07bbcc1ee2cd1580ffbe415e04
jp6ru8958958 <jp6ru8958958@gmail.com>
2019-04-13 18:29:30 +0800

    US_NAE update

--- US_Non-Agricultural_Employment_Population.py
+++ US_Non-Agricultural_Employment_Population.py
@@ -4,17 +4,15 @@ from bs4 import BeautifulSoup
 url='https://tradingeconomics.com/united-states/non-farm-payrolls'
 Response=requests.get(url)
 Response.encoding='Utf-8'
-print(Response.status_code)
+#print(url)
+#print(Response.status_code)
 #print(Response.text)
 soup=BeautifulSoup(Response.text,"html.parser")
-find=soup.select("td.datatable-item.datatable-item-positive")
+find=soup.select("td.datatable-item.datatable-item-positive")#數據
 
-for blank_scan in range(len(find)):
-    if find[blank_scan] == '''<td class="datatable-item datatable-item-positive" id="actual"></td>''':
-        find[blank_scan].text = "None"
-
+print("  Actual  Previous 	Consensus TEForecast ")
 for row in range(7):
-    print("---")
+    print("--------------------------------------------")
     print_=row*4
-    print(str.strip(find[print_].text),str.strip(find[print_+1].text),str.strip(find[print_+2].text),str.strip(find[print_+3].text))
-print("---")
+    print("%7s  %7s   %7s    %7s"%(str.strip(find[print_].text),str.strip(find[print_+1].text),str.strip(find[print_+2].text),str.strip(find[print_+3].text)))
+
