344bda7a9ff9466eda585b7b62da6356e95ba9ca
jp6ru8958958 <jp6ru8958958@gmail.com>
2019-04-15 19:19:16 +0800

    PPI_

--- Producer_Price_Index.py
+++ Producer_Price_Index.py
@@ -5,20 +5,19 @@ url = 'http://statdb.dgbas.gov.tw/pxweb/Dialog/varval.asp?ma=PR0101A1M&ti=%AE%F8
 driver = webdriver.Firefox()
 driver.get(url)
 driver.find_element_by_link_text(u"全選").click()
-# ERROR: Caught exception [ERROR: Unsupported command [addSelection | name=values2 | label=總指數]]
 driver.find_element_by_xpath(
     u"(.//*[normalize-space(text()) and normalize-space(.)='搜尋 文件開始'])[1]/preceding::option[83]").click()
-# ERROR: Caught exception [ERROR: Unsupported command [addSelection | name=values3 | label=原始值]]
 driver.find_element_by_xpath(
     u"(.//*[normalize-space(text()) and normalize-space(.)='搜尋 文件開始'])[1]/preceding::option[2]").click()
 driver.find_element_by_name("sel").click()
 html = driver.page_source
 # print(html)
+
 soup = BeautifulSoup(html, "html.parser")
 PPI = soup.select("td", nowrap="")
-print("",end="          ")
+print("", end="          ")
 for print_M in range(12):
-    print("M%2d"%(print_M+1),end="      ")
+    print("M%2d"%(print_M+1), end="      ")
 print("")
 for print_ in range(39):
     move = print_ * 24 + 5
