25e7cce1bc1e79651067f82c0bcdce1f78d8daeb
jp6ru8958958 <jp6ru8958958@gmail.com>
2019-04-15 19:16:39 +0800

    PPI

--- Producer_Price_Index.py
+++ Producer_Price_Index.py
@@ -1,10 +1,28 @@
-#生產者物價指數
-import requests
+# 生產者物價指數
+from selenium import webdriver
 from bs4 import BeautifulSoup
-url=''
-Response=requests.get(url)
-Response.encoding='Utf-8'
-#print(Response.status_code)
-#print(Response.text)
-soup=BeautifulSoup(Response.text,"html.parser")
-#find=soup.select("")
+url = 'http://statdb.dgbas.gov.tw/pxweb/Dialog/varval.asp?ma=PR0101A1M&ti=%AE%F8%B6O%AA%CC%AA%AB%BB%F9%B0%F2%A5%BB%A4%C0%C3%FE%AB%FC%BC%C6-%A4%EB&path=../PXfile/PriceStatistics/&lang=9&strList=L'
+driver = webdriver.Firefox()
+driver.get(url)
+driver.find_element_by_link_text(u"全選").click()
+# ERROR: Caught exception [ERROR: Unsupported command [addSelection | name=values2 | label=總指數]]
+driver.find_element_by_xpath(
+    u"(.//*[normalize-space(text()) and normalize-space(.)='搜尋 文件開始'])[1]/preceding::option[83]").click()
+# ERROR: Caught exception [ERROR: Unsupported command [addSelection | name=values3 | label=原始值]]
+driver.find_element_by_xpath(
+    u"(.//*[normalize-space(text()) and normalize-space(.)='搜尋 文件開始'])[1]/preceding::option[2]").click()
+driver.find_element_by_name("sel").click()
+html = driver.page_source
+# print(html)
+soup = BeautifulSoup(html, "html.parser")
+PPI = soup.select("td", nowrap="")
+print("",end="          ")
+for print_M in range(12):
+    print("M%2d"%(print_M+1),end="      ")
+print("")
+for print_ in range(39):
+    move = print_ * 24 + 5
+    print(1981+print_, end="  ")
+    for print_PPI in range(12):
+        print("%7s"%(PPI[move+print_PPI*2].text), end="  ")
+    print("")
